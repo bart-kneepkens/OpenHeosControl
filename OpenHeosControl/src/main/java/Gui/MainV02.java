cd689a1c24c7c3a6ea1ca33657c9a9a3f8d363cd
Bart Kneepkens <bkneepkens@msn.com>
2016-10-25 23:19:03 +0200

    Native UI because it looks better.

--- OpenHeosControl/src/main/java/Gui/MainV02.java
+++ OpenHeosControl/src/main/java/Gui/MainV02.java
@@ -157,18 +157,6 @@ public class MainV02 extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Metal look and feel */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Metal".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainV02.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
